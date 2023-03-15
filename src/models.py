5983cd17d857274c6a52bf4ce7348a629a2166b4
dedalovitor <dedalovitor@gmail.com>
2023-03-15 04:53:05 +0000

    uploading my code

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    gender = Column(String(250))
+    species = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    type = Column(String(250))
+    dimension = Column(String(250))
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+class Episode(Base):
+    __tablename__ = 'episode'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    air_date = Column(String(250))
+    episode = Column(String(250))
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+    location_id = Column(Integer, ForeignKey('location.id'))
+    location = relationship(Location)
+    episode_id = Column(Integer, ForeignKey('episode.id'))
+    episode = relationship(Episode)
+
+class LocationsAndEpisodes(Base):
+    __tablename__ = 'locationsandepisodes'
+    id = Column(Integer, primary_key=True)
+    location_id = Column(Integer, ForeignKey('location.id'))
+    location = relationship(Location)
+    episode_id = Column(Integer, ForeignKey('episode.id'))
+    episode = relationship(Episode)
+
+class EpisodesAndCharacters(Base):
+    __tablename__ = 'episodesandcharacters'
+    id = Column(Integer, primary_key=True)
+    episode_id = Column(Integer, ForeignKey('episode.id'))
+    episode = relationship(Episode)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
